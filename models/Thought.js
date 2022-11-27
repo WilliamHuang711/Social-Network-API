daff57f85dd54be014c1f74596e8682d2f480cfc
William Huang <hh921088586@berkeley.edu>
2022-11-27 01:33:50 -0800

    added models and routes

---
+++ models/Thought.js
@@ -0,0 +1,62 @@
+const { Schema, model, Types } = require("mongoose");
+
+const ReactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+
+  username: {
+    type: String,
+    required: true,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // Use a getter method to format the timestamp on query
+  },
+});
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Thought is Required",
+      minlength: 1,
+      maxlength: 280,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
